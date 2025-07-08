e87862d098b6ab2330955a090c78c8c85fe0e4d5
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-07-08 21:59:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 153.94 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,56 +1,44 @@
 import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.HashSet;
-import java.util.Arrays;
+import java.util.Map;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         
-        // 신고 당한 횟수
-        HashMap<String, Integer> reportMap = new HashMap<>();
+        // 신고 당한 유저
+        HashMap<String, HashSet<String>> reportedUser = new HashMap<>();
         
-        // 신고자
-        HashMap<String, ArrayList<String>> reporterMap = new HashMap<>();
+        // 처리 결과 메일을 받은 유저
+        HashMap<String, Integer> count = new HashMap<>();
         
-        // 활동제한자 (k번 이상 신고 받은 사람)
-        ArrayList<String> restricted = new ArrayList<String>();
-        
-        // 리턴
-        int[] result = new int[id_list.length];
-        
-        // 리포트 중복 제거
-        Set<String> uniq = new HashSet<>(Arrays.asList(report));
-        
-        for (String s: uniq) {
-            String reporter = s.split(" ")[0];
-            String reported = s.split(" ")[1];
-            ArrayList<String> list = reporterMap.getOrDefault(reporter, new ArrayList<>());
-            reportMap.put(reported, reportMap.getOrDefault(reported, 0) + 1);
-            list.add(reported);
-            reporterMap.put(reporter, list);
+        // 신고 기록 순회
+        for (String r : report) {
+            String[] s = r.split(" ");
+            String userId = s[0];
+            String reportedId = s[1];
+
+            if (!reportedUser.containsKey(reportedId)) { // 신고 당한 기록이 없다면
+                reportedUser.put(reportedId, new HashSet<>());    
+            }       
+            // 신고한 사람의 아이디를 해시맵의 value인 해시셋에 추가
+            reportedUser.get(reportedId).add(userId);
         }
         
-        for (String s: id_list) {
-            if (reportMap.getOrDefault(s, 0) >= k) {
-                restricted.add(s);
-            }
-        }
-        
-        for (int i=0; i<id_list.length; i++) {
-            Integer mailCount = 0;
-            for (String reported: reporterMap.getOrDefault(id_list[i], new ArrayList<>())) {
-                if (restricted.contains(reported)) {
-                    mailCount++;
+        for (Map.Entry<String, HashSet<String>> entry: reportedUser.entrySet()) {
+            if (entry.getValue().size() >= k) {
+                for (String uid : entry.getValue()) {
+                    count.put(uid, count.getOrDefault(uid, 0) + 1);
                 }
             }
-            result[i] = mailCount;
         }
         
+        int[] answer = new int[id_list.length];
         
+        // 각 아이디별 메일을 받은 횟수를 순서대로 정리
+        for (int i = 0; i < id_list.length; i++) {
+            answer[i] = count.getOrDefault(id_list[i], 0);
+        }
         
-        
-        return result;
+        return answer;
     }
 }
