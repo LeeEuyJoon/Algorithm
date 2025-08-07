db56644f93a590c04975fca3840afa2c83cb6f15
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-08-07 23:25:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 283.12 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -5,22 +5,18 @@ import java.util.Map;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         
-        // 신고 당한 유저
         HashMap<String, HashSet<String>> reportedUser = new HashMap<>();
         
-        // 처리 결과 메일을 받은 유저
         HashMap<String, Integer> count = new HashMap<>();
         
-        // 신고 기록 순회
         for (String r : report) {
             String[] s = r.split(" ");
             String userId = s[0];
             String reportedId = s[1];
 
-            if (!reportedUser.containsKey(reportedId)) { // 신고 당한 기록이 없다면
+            if (!reportedUser.containsKey(reportedId)) { 
                 reportedUser.put(reportedId, new HashSet<>());    
             }       
-            // 신고한 사람의 아이디를 해시맵의 value인 해시셋에 추가
             reportedUser.get(reportedId).add(userId);
         }
         
@@ -34,7 +30,6 @@ class Solution {
         
         int[] answer = new int[id_list.length];
         
-        // 각 아이디별 메일을 받은 횟수를 순서대로 정리
         for (int i = 0; i < id_list.length; i++) {
             answer[i] = count.getOrDefault(id_list[i], 0);
         }
