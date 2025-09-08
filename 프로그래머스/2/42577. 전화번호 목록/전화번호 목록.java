ee5b887825eb7b944a7f5e3b6488434ccb26d0db
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-09-08 22:03:36 +0900

    [level 2] Title: 전화번호 목록, Time: 386.29 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,13 +1,17 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 전화번호부 정렬
+
+        // 폰북을 사전순으로 정렬하고
         Arrays.sort(phone_book);
-        // 전화번호부에서 연속된 두 개의 전화번호 비교
-        for (int i = 0; i < phone_book.length -1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i]))
-                return false;
+        
+        // 인접한 폰번호만 접두어 검사
+        for ( int i = 0; i < phone_book.length-1; i++ ) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;   
+            }
         }
         
         return true;
