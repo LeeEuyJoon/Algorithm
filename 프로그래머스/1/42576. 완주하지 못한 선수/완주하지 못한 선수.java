40045061fd1834feacdcfd9d23ef18ce649bc3f4
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-09-08 21:37:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.05 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,18 +2,23 @@ import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        HashMap<String, Integer> map =  new HashMap<>();
         
-        for (String string: completion) {
-            hashMap.put(string, hashMap.getOrDefault(string, 0) + 1);
+        // map 에 참가자를 map에 담으며 value 플러스
+        for (String s: participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         
-        for (String string: participant) {
-            if (hashMap.getOrDefault(string, 0) == 0) {
-                return string;
-            }
-            hashMap.put(string, hashMap.get(string) - 1);
+        // 완주자를 순회하며 value 마이너스
+        for (String s: completion) {
+            map.put(s, map.get(s) - 1);
         }
-        return null;
+        
+        // map을 순회하며 value가 0이 아니면 리턴
+        for (String key: map.keySet()) {
+            if (!map.get(key).equals(0)) return key;
+        }
+        
+        return "";
     }
 }
