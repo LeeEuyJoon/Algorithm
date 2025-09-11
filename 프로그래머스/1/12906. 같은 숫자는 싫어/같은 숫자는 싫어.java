d0cdb5615c016de57fac5c97fe5661c19813615c
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-09-12 02:35:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.88 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        int basket = Integer.MIN_VALUE;    
+        for (Integer i : arr) {            
+            if (i.equals(basket)) continue;
+            deque.add(i);
+            basket = i;
+        }
+
+        return deque.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
