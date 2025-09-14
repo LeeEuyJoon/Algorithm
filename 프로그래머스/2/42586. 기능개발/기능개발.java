b478629635e95b2691590eaed676b8208cd43069
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-09-14 21:46:19 +0900

    [level 2] Title: 기능개발, Time: 2.39 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,20 +3,23 @@ import java.util.ArrayDeque;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        
+        // 1. 각 작업의 배포 가능일 게산
+        // 2. 배포 가능일 배열을 for문 돌려서 이전 값보다 작으면 count + 1
+        
         Queue<Integer> answer = new ArrayDeque<>();
         
         int n = progresses.length;
-        
-        // 각 작업의 배포 가능일 계산
         int[] daysLeft = new int[n];
-        for (int i = 0; i < n; i++) {
+        
+        for (int i = 0; i<n; i++) {
             daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
         }
         
         int count = 0;
         int maxDay = daysLeft[0];
         
-        for (int i = 0; i < n; i++) {
+        for (int i=0; i<n; i++) {
             if (daysLeft[i] <= maxDay) {
                 count++;
             }
@@ -28,6 +31,7 @@ class Solution {
         }
         
         answer.add(count);
+        
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
