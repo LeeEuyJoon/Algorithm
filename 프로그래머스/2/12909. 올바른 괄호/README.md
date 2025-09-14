f617c94e68e3366563e91f815b1432e6e68096c4
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-09-14 22:08:53 +0900

    [level 2] Title: 올바른 괄호, Time: 24.98 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 54.9 MB, 시간: 18.36 ms
+메모리: 55 MB, 시간: 24.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 29일 17:49:57
+2025년 09월 14일 22:08:50
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,26 @@ import java.util.ArrayDeque;
 
 class Solution {
     boolean solution(String s) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-    
-        char[] a = s.toCharArray();
-        for (char c : a) {
-            if (c == '(') {
-                stack.push(c);
-            }
-            else {
-                if (stack.isEmpty() || stack.pop() == c) {
-                    return false;
-                }
+        
+        // 첫 문자가 "("인지 ")"인지 검사
+        // 마지막 문자가 "("인지 ")"인지 검사
+        // 각 문자를 for문 돌면서 "(" 이거면 push ")" 이거면 pop
+        // size가 0이면 트루
+        
+        if (s.charAt(0) == ')' || s.charAt(s.length() - 1) == '(') return false;
+ 
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(0);
+            else if (c == ')') {
+                if (stack.isEmpty()) { return false; }
+                stack.pop();
             }
         }
-            return stack.isEmpty();
+        
+        return stack.size() == 0 ? true : false;
+        
     }
+
 }
