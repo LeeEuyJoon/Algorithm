a74f36381e304304a07163e251c241daa32fddc1
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-09-16 03:59:53 +0900

    [level 2] Title: 주식가격, Time: 19.50 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,24 +2,15 @@ import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-        
-        // 스택에는 가격이 떨어지지 않는 기간 저장
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        
-        for (int i = 1; i < n; i++) {
-            // 가격이 떨어지면 이전 가격 기간 계산
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int j = stack.pop();
-                answer[j] = i - j;
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
             }
-            stack.push(i);
-        }
-        while (!stack.isEmpty()) {
-            int j = stack.pop();
-            answer[j] = n - 1 - j;
         }
         return answer;
     }
