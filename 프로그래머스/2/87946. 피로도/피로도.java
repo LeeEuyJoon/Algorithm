52b809e739029f8596794ad042e8f3a307c8194d
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-10-11 07:34:15 +0900

    [level 2] Title: 피로도, Time: 2.80 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,32 +1,33 @@
 class Solution {
+    
     private static int answer;
-    private static int[][] Dungeons;
     private static boolean[] visited;
+    private static int[][] Dungeons;
 
     
-    // DFS
+    // 순회하면서 최소 피로도 이하로 k 가 떨어지면 continue
     private static void backtrack(int k, int cnt) {
         for (int i = 0; i < Dungeons.length; i++) {
-            // 현재 피로도가 i번째 던전의 최소 필요 피로도보다 크거나 같고,
-            // i번째 던전을 방문한 적이 없다면
             if (!visited[i] && k >= Dungeons[i][0]) {
-                visited[i] = true; // i번째 던전을 방문 처리
-                // 현재까지의 최대 탐험 가능 던전 수와
-                // i번째 던전에서 이동할 수 있는 최대 탐험 가능 던전 수 중 큰 값을 선택하여 업데이트
+                visited[i] = true;
                 backtrack(k - Dungeons[i][1], cnt + 1);
                 answer = Math.max(answer, cnt + 1);
-                visited[i] = false; // i번째 던전을 다시 방문 취소
+                visited[i] = false;
             }
         }
+        
     }
     
     public int solution(int k, int[][] dungeons) {
+        // dfs 하면 되지 않을까??
+        
         answer = 0;
         Dungeons = dungeons;
-        // 던전 방문 여부를 저장할 배열
         visited = new boolean[dungeons.length];
         
         backtrack(k, 0);
+        
         return answer;
+        
     }
 }
