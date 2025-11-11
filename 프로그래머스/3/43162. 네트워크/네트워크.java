fee89541a120185d3eb4bd26d5b552aca23492e3
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-11-12 01:57:17 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,27 @@
 class Solution {
-    private static boolean[] visit;
     private static int[][] computer;
+    private static boolean[] visited;
     
-    private static void dfs(int now) {
-        visit[now] = true; // 현재 노드 방문 처리
-        for (int i = 0; i < computer[now].length; i++) {
-            // 연결되어 있으며 방문하지 않은 노드라면
-            if (computer[now][i] == 1 && !visit[i]) {
-                dfs(i); // 해당 노드를 방문하러 이동
+    private static void dfs(int n) {
+        visited[n] = true;
+        for (int i = 0; i < computer[n].length; i++) {
+            if (computer[n][i] == 1 && visited[i] == false) {
+                dfs(i);
             }
-        }
-    }
+        }       
+    }   
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         computer = computers;
-        visit = new boolean[n]; // 방문 여부
+        visited = new boolean[n];
         
-        for (int i=0; i < n; i++) {
-            if (!visit[i]) { // 아직 방문하지 않은 노드라면 해당 노드를 시작으로 dfs 진행
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 dfs(i);
                 answer++;
             }
         }
-        
-        
         return answer;
     }
 }
