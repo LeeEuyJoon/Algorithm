8f0ceff52ab58358815fcddd9909ece9f073039b
LeeEuyJoon <94832884+LeeEuyJoon@users.noreply.github.com>
2025-11-11 22:30:32 +0900

    [level 3] Title: N으로 표현, Time: 28.32 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,22 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if (N == number) return 1;
-
+        // N을 i번 사용했을 때 만들 수 있는 수의 집합
         List<Set<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) dp.add(new HashSet<>());
 
-        dp.get(1).add(N);
+        for (int i = 0; i <= 8; i++) {
+            dp.add(new HashSet<>());
+        }
 
-        for (int i = 2; i <= 8; i++) {
-            // 이어붙인 수 (예: 55, 555, 5555)
-            int concat = Integer.parseInt(String.valueOf(N).repeat(i));
-            dp.get(i).add(concat);
+        for (int i = 1; i <= 8; i++) {
+            // N을 i번 이어붙인 숫자 (예: 5, 55, 555)
+            int repeated = Integer.parseInt(String.valueOf(N).repeat(i));
+            dp.get(i).add(repeated);
 
-            // 가능한 조합 계산
+            // 이전 단계들의 조합으로 dp[i] 채우기
             for (int j = 1; j < i; j++) {
-                for (int a : dp.get(j)) {
-                    for (int b : dp.get(i - j)) {
+                Set<Integer> set1 = dp.get(j);
+                Set<Integer> set2 = dp.get(i - j);
+
+                for (int a : set1) {
+                    for (int b : set2) {
                         dp.get(i).add(a + b);
                         dp.get(i).add(a - b);
                         dp.get(i).add(a * b);
@@ -26,10 +29,13 @@ class Solution {
                 }
             }
 
-            // 목표 수 찾으면 조기 종료
-            if (dp.get(i).contains(number)) return i;
+            // 이번 단계에서 number 찾으면 바로 반환
+            if (dp.get(i).contains(number)) {
+                return i;
+            }
         }
 
-        return -1; // 8 이상이면 불가능
+        // 8번까지 못 찾으면 -1
+        return -1;
     }
 }
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
 
 ### 성능 요약
 
-메모리: 85.9 MB, 시간: 29.17 ms
+메모리: 92.9 MB, 시간: 28.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 16일 23:13:35
+2025년 11월 11일 22:30:27
 
 ### 문제 설명
 
